d9d207d5d415276051621173b8f4c84a947ab8da
tanvishetty <tanvishetty014@gmail.com>
2025-06-08 10:58:05 +0530

    second commit

---
+++ code2.java
@@ -0,0 +1,12 @@
+package FORPRACTICAL;
+
+public class code2 {
+    String name = "Tanvi";
+    int age = 10;
+    public void details()
+    {
+        System.out.println("Name:"+ name);
+        System.out.println("Age:"+ age);
+    }
+
+}
