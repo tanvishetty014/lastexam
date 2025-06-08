af4b51f1aadc2033f86af32eab9b6ac20c2f6c9d
tanvishetty <tanvishetty014@gmail.com>
2025-06-08 10:54:13 +0530

    first commit

---
+++ code1.java
@@ -0,0 +1,10 @@
+package FORPRACTICAL;
+
+public class code1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        code2 c =  new code2();
+        c.details();
+    }
+
+}
